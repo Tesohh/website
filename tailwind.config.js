6744bc71d1d966c13fe06ae3c2dea61a265ddd56
Tesohh <simozilla006@gmail.com>
2022-01-03 00:32:31 +0100

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00cc94",
+        graybg: "#222733",
+      },
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+        mono: ["JetBrains Mono", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
