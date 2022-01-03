de145b30e6a5288bdba9a631e24f6d0be2105f35
Tesohh <simozilla006@gmail.com>
2022-01-03 20:10:31 +0100

    pasd

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     extend: {
       colors: {
         primary: "#00cc94",
+        secondary: "#bbc0c9", // "#a1a5ad",
         textdim: "#4f5666",
         graybg: "#222733",
         // navbg: "#222733",
