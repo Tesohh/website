f783de0b1350432386f5051a0b3825758b6506e8
Tesohh <simozilla006@gmail.com>
2022-01-03 16:04:02 +0100

    hi mi name is

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     extend: {
       colors: {
         primary: "#00cc94",
+        textdim: "#4f5666",
         graybg: "#222733",
+        // navbg: "#222733",
       },
       fontFamily: {
         sans: ["Lato", "sans-serif"],
